acf603d69ef1a3653cd6261b4a420942584ac308
Scott <scott5971@gmail.com>
2018-08-22 17:52:29 +0800

    add web route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('student/{student_no}', function($student_no) {
+	return "學號 : $student_no";
+});
+
+Route::get('student/{student_no}/score', function($student_no) {
+	return "學號 : $student_no 的所有成績";
+});
+
+
+Route::get('student/{student_no}/score/{subject}', function($student_no, $subject) {
+	return "學號 : $student_no <br> 科目 : $subject 的成績";
+});
